afd2ac0f1270762b426754790472035e662f828d
Padmanabhannm18 <152405762+Padmanabhannm18@users.noreply.github.com>
2025-03-06 13:34:53 +0530

    Delete predict.py

--- predict.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import streamlit as st
-
-def predict_stock_price(model, scaler, input_features):
-    """Predicts stock price based on user input."""
-    scaled_data = scaler.transform([input_features])
-    prediction = model.predict(scaled_data)
-    return prediction[0]
