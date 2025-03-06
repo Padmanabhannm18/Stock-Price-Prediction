fc71617a7f9cd18fe68b2280778561f59bc7c4ae
Padmanabhannm18 <152405762+Padmanabhannm18@users.noreply.github.com>
2025-03-06 13:43:29 +0530

    Update data_fetch.py

--- data_fetch.py
+++ data_fetch.py
@@ -1,14 +1,8 @@
 import yfinance as yf
-import pandas as pd
 
-def get_realtime_stock_data(ticker):
-    try:
-        stock = yf.Ticker(ticker)
-        data = stock.history(period="1mo")  # Fetch last 30 days data
-        if not data.empty:
-            return data
-        else:
-            return pd.DataFrame()
-    except Exception as e:
-        print(f"Error fetching stock data: {e}")
-        return pd.DataFrame()
+def get_sp500_tickers():
+    import pandas as pd
+    url = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
+    table = pd.read_html(url)
+    sp500_tickers = table[0]["Symbol"].tolist()
+    return sp500_tickers
