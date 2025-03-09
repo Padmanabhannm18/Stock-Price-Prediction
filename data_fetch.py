fdda28b1d0827b0f9f97f53b1419dbf53ff987d2
Padmanabhannm18 <152405762+Padmanabhannm18@users.noreply.github.com>
2025-03-09 11:40:22 +0530

    Update data_fetch.py

--- data_fetch.py
+++ data_fetch.py
@@ -1,11 +1,20 @@
 import yfinance as yf
+import pandas as pd
 
 def get_sp500_tickers():
-    import pandas as pd
     url = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
     table = pd.read_html(url)
     sp500_tickers = table[0]["Symbol"].tolist()
     return sp500_tickers
+
 def fetch_stock_data(ticker):
     stock = yf.Ticker(ticker)
-    return stock.history(period="1y")
+    history = stock.history(period="1y")
+
+    if not history.empty:
+        last_updated = history.index[-1].strftime("%Y-%m-%d")
+        print(f"Last updated date for {ticker}: {last_updated}")
+    else:
+        print(f"No data available for {ticker}")
+
+    return history
